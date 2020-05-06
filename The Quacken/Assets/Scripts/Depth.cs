04ffb5ce97d241d702adbbd8a8af07b5d6b34581
Ruthenium <rutaspk@gmail.com>
2020-05-06 15:20:10 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ The Quacken/Assets/Scripts/Depth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depth : MonoBehaviour
+{
+    SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sortingOrder = 12;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "mapSort")
+        {
+            renderer.sortingOrder = 9;
+        }
+        if (collision.tag == "wallSort")
+        {
+            renderer.sortingOrder = 2;
+        }
+        if (collision.tag == "tunnel")
+        {
+            renderer.color = Color.clear;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        renderer.sortingOrder = 12;
+
+        if (collision.tag == "tunnel")
+        {
+            renderer.color = Color.white;
+        }
+    }
+}
