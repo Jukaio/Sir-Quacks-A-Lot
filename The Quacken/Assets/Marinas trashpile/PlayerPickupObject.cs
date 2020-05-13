13d38e6195cf80e8c0b85f609cc477b297984f37
Marina Arknell <marina.arknell@gmail.com>
2020-05-13 11:14:04 +0200

    pick up scripts if we use it

---
+++ The Quacken/Assets/Marinas trashpile/PlayerPickupObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickupObject : MonoBehaviour
+{
+    public GameObject currInterObj = null;
+    private bool isHolding = false;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currInterObj.SendMessage("DoInteraction");
+            isHolding = true;
+           
+        }
+        
+    }
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("pickupObject"))
+        {
+            Debug.Log("You can now pick up the object!");
+            currInterObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("pickupObject"))
+        {
+            if(other.gameObject == currInterObj)
+            {
+                currInterObj = null;
+            }
+        }
+    }
+}
---
+++ The Quacken/Assets/grabThrowScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabThrowScript : MonoBehaviour
+{
+    public bool grabbed;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public Transform holdpoint;
+    public float throwForce;
+    public LayerMask notPickUpObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!grabbed)
+            {
+                Physics2D.queriesStartInColliders = false;
+                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
+                
+                if (hit.collider != null && hit.collider.tag=="pickupObject")
+                {
+                    grabbed = true;
+                }
+               
+                //can grab
+            }
+            else if(!Physics2D.OverlapPoint(holdpoint.position, notPickUpObject))
+            {
+                grabbed = false;
+
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwForce;
+                }
+                //throw
+            }
+        }
+        if (grabbed)
+        {
+            hit.collider.gameObject.transform.position = holdpoint.position;
+            //grabbing
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position, transform.position+Vector3.right * transform.localScale.x*distance);
+    }
+}
